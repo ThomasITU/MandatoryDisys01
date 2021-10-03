3c8ff0bd3bd48f249543ed4e0dc0ad7ec7f80a2d
Thomas Wolgast Rørbech <thwr@itu.dk>
2021-10-03 03:34:12 +0200

    add evalRequest
    add CLI

--- client/client.go
+++ client/client.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
-	"os"
-	"time"
 
 	co "github.com/ThomasITU/MandatoryDisys01/course"
 	"google.golang.org/grpc"
 )
 
 const (
-	address     = "localhost:50051"
-	defaultName = "courseClient"
+	address        = "localhost:50051"
+	defaultRequest = "allCourses"
 )
 
 func main() {
@@ -26,43 +25,73 @@ func main() {
 	c := co.NewCourseServiceClient(conn)
 
 	// Contact the server and print out its response.
-	request := defaultName
-	if len(os.Args) > 1 {
-		request = os.Args[1]
+	request := defaultRequest
+
+	for {
+		fmt.Scanln(&request)
+		ctx := context.Background()
+
+		evalRequest(request, ctx, c)
+		request = ""
 	}
+}
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+func evalRequest(request string, ctx context.Context, c co.CourseServiceClient) {
+	var parameters string
+	fmt.Scanln(&parameters)
 
+	switch request {
+	case "put":
+		put(parameters, ctx, c)
+	case "allCourses":
+		allCourses(ctx, c)
+	case "getCourse":
+		getCourse(parameters, ctx, c)
+	case "deleteCourse":
+		deleteCourse(parameters, ctx, c)
+	case "postCourse":
+		postCourse(parameters, ctx, c)
+	default:
+		break
+	}
+}
+
+func allCourses(ctx context.Context, c co.CourseServiceClient) {
 	allCourses, err := c.GetCourses(ctx, &co.GetCoursesRequest{})
 	if err != nil {
 		log.Fatalf("could not get all courses: %v", err)
 	}
 	log.Printf("%s", allCourses.GetName())
+}
 
-	// getCourse, err := c.GetCourseById(ctx, &co.GetCourseByIdRequest{Request: request})
-	// if err != nil {
-	// 	log.Fatalf("could not getCourse: %s", request)
-	// }
-	// log.Printf("%s", getCourse.GetName())
-
-	// deleteCourse, err := c.DeleteCourseById(ctx, &co.DeleteCourseByIdRequest{Request: request})
-	// if err != nil {
-	// 	log.Fatalf("could not delete: %s", request)
-	// }
-	// log.Printf("%s", deleteCourse.GetName())
-
-	// postCourse, err := c.PostCourse(ctx, &co.PostCourseRequest{Request: request})
-	// if err != nil {
-	// 	log.Fatalf("could not post course %s", request)
-	// }
-	// log.Printf("%s", postCourse.GetName())
-
-	putCourse, err := c.PutCourse(ctx, &co.PutCourseRequest{Request: request})
+func getCourse(params string, ctx context.Context, c co.CourseServiceClient) {
+	getCourse, err := c.GetCourseById(ctx, &co.GetCourseByIdRequest{Request: params})
 	if err != nil {
-		log.Fatalf("could not update course: %s", request)
+		log.Fatalf("could not getCourse: %s", params)
+	}
+	log.Printf("%s", getCourse.GetName())
+}
+
+func deleteCourse(params string, ctx context.Context, c co.CourseServiceClient) {
+	deleteCourse, err := c.DeleteCourseById(ctx, &co.DeleteCourseByIdRequest{Request: params})
+	if err != nil {
+		log.Fatalf("could not delete: %s", params)
+	}
+	log.Printf("%s", deleteCourse.GetName())
+}
+
+func postCourse(params string, ctx context.Context, c co.CourseServiceClient) {
+	postCourse, err := c.PostCourse(ctx, &co.PostCourseRequest{Request: params})
+	if err != nil {
+		log.Fatalf("could not post course %s", params)
+	}
+	log.Printf("%s", postCourse.GetName())
+}
+
+func put(params string, ctx context.Context, c co.CourseServiceClient) {
+	putCourse, err := c.PutCourse(ctx, &co.PutCourseRequest{Request: params})
+	if err != nil {
+		log.Fatalf("could not update course: %s", params)
 	}
 	log.Printf("%s", putCourse.GetName())
-
-	
 }
